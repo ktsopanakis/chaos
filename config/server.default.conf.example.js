a8e13f917ae2d4bcfb67a9223cfd33eca36e82f8
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2016-03-21 21:08:13 +0200

    adding some backbone to it

---
+++ config/server.default.conf.example.js
@@ -0,0 +1,26 @@
+var ld = require('lodash');
+
+var config = {};
+
+config.dev = {
+  //environment
+  env: 'dev',
+  //web server settigns
+  serverPort: process.env.port || 1337,
+  // other settings
+  other: {}
+};
+
+// config when running in build mode
+config.build = ld.cloneDeep(config.dev);
+config.build.env = 'build';
+
+// configuration for pandora ci
+config.test = ld.cloneDeep(config.dev);
+config.test.env = 'test';
+
+// config when running on a production server (NODE_ENV=production)
+config.production = ld.cloneDeep(config.dev);
+config.production.env = 'production';
+
+module.exports = config;
