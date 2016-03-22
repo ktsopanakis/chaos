d8886b083bbf9bfd02d15941db6068adb72efe87
Kostas Tsopanakis <Kostas Tsopanakis>
2016-03-22 03:40:51 +0200

    rebuilding base, on a more solid ground

--- config/gulp.conf.js
+++ config/gulp.conf.js
@@ -2,6 +2,7 @@ module.exports = function() {
   var temp = './.tmp/';
   var build = './.build/';
   var client = './src/client/';
+  var clientApp = client + 'app/';
 
 
   var config = {
@@ -10,14 +11,55 @@ module.exports = function() {
       './config/**.js'
     ],
     build: build,
+    client: client,
+    index: client + 'index.html',
     temp: temp,
     fonts: [
       './bower_components/bootstrap/fonts/**/*.*',
-      'fonts/**/*.*'
+      client + 'fonts/**/*.*'
     ],
     images: client + 'images/**/*.*',
     css: temp + 'styles.css',
     less: client + 'styles/styles.less',
+    bower: {
+      json: require('../bower.json'),
+      directory: './bower_components/',
+      ignorePath: '../..'
+    },
+    js: [
+      client + '**/*.js',
+      '!' + client + '**/*.spec.js'
+    ],
+    /**
+     * optimized files
+     */
+    optimized: {
+      app: 'app.js',
+      lib: 'lib.js'
+    },
+
+    htmltemplates: clientApp + '**/*.html',
+    /**
+     * template cache
+     */
+    templateCache: {
+      file: 'templates.js',
+      options: {
+        module: 'app.core',
+        standAlone: false,
+        root: 'app/'
+      }
+    },
+  };
+
+  config.getWiredepDefaultOptions = function() {
+    var options = {
+      bowerJson: config.bower.json,
+      directory: config.bower.directory,
+      ignorePath: config.bower.ignorePath
+    };
+
+    return options;
   };
 
   return config;
--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,16 @@ var gulpImagemin = require('gulp-imagemin');
 var gulpPlumber = require('gulp-plumber');
 var gulpLess = require('gulp-less');
 var gulpAutoprefixer = require('gulp-autoprefixer');
+var gulpInject = require('gulp-inject');
+var gulpMinifyHtml = require('gulp-minify-html');
+var gulpAngularTemplatecache = require('gulp-angular-templatecache');
+var gulpFilter = require('gulp-filter');
+var gulpUseref = require('gulp-useref');
+var gulpCsso = require('gulp-csso');
+var gulpUglify = require('gulp-uglify');
+var gulpNgAnnotate = require('gulp-ng-annotate');
+var gulpRev = require('gulp-rev');
+var gulpRevReplace = require('gulp-rev-replace');
 
 // display all possible tasks
 gulp.task('help', gulpTaskListing);
@@ -95,4 +105,84 @@ gulp.task('styles', ['clean-styles'], function() {
       browsers: ['last 2 version', '> 5%']
     }))
     .pipe(gulp.dest(config.temp));
+});
+
+// create angular templatecache
+gulp.task('templatecache', ['clean-code'], function() {
+  helpers.log('Creating AngularJS $templateCache');
+  return gulp
+    .src(config.htmltemplates)
+    .pipe(gulpIf(args.verbose, gulpPrint()))
+    .pipe(gulpMinifyHtml({
+      empty: true
+    }))
+    .pipe(gulpAngularTemplatecache(
+      config.templateCache.file,
+      config.templateCache.options
+    ))
+    .pipe(gulp.dest(config.temp));
+});
+
+//TODO: bootstrap needs manually adding to the bower.jsopn the css file
+// adding dependancies
+gulp.task('wiredep', function() {
+  helpers.log('Wire up the bower css js and our app js into the html');
+  var options = config.getWiredepDefaultOptions();
+  var wiredep = require('wiredep').stream;
+  return gulp
+    .src(config.index)
+    .pipe(gulpIf(args.verbose, gulpPrint()))
+    .pipe(wiredep(options))
+    .pipe(gulpInject(gulp.src(config.js)))
+    .pipe(gulp.dest(config.client));
+});
+gulp.task('inject', ['wiredep', 'styles', 'templatecache'], function() {
+  helpers.log('Wire up the app css into the html, and call wiredep ');
+  return gulp
+    .src(config.index)
+    .pipe(gulpIf(args.verbose, gulpPrint()))
+    .pipe(gulpInject(gulp.src(config.css)))
+    .pipe(gulp.dest(config.client));
+});
+
+gulp.task('test', ['vet', 'templatecache'], function(done) {
+//  startTests(true /* singleRun */ , done);
+});
+
+gulp.task('optimize', ['inject', 'test'], function() {
+  helpers.log('Optimizing the javascript, css, html');
+  var assets = gulpUseref.assets({
+    searchPath: './'
+  });
+  var templateCache = config.temp + config.templateCache.file;
+  var cssFilter = gulpFilter('**/*.css');
+  var jsLibFilter = gulpFilter('**/' + config.optimized.lib);
+  var jsAppFilter = gulpFilter('**/' + config.optimized.app);
+  return gulp
+    .src(config.index)
+    .pipe(gulpPlumber())
+    .pipe(gulpInject(
+      gulp.src(templateCache, {
+        read: false
+      }), {
+        starttag: '<!-- inject:templates:js -->'
+      }))
+    .pipe(assets)
+    .pipe(cssFilter)
+    .pipe(gulpCsso())
+    .pipe(cssFilter.restore())
+    .pipe(jsLibFilter)
+    .pipe(gulpUglify())
+    .pipe(jsLibFilter.restore())
+    .pipe(jsAppFilter)
+    .pipe(gulpNgAnnotate())
+    .pipe(gulpUglify())
+    .pipe(jsAppFilter.restore())
+    .pipe(gulpRev())
+    .pipe(assets.restore())
+    .pipe(gulpUseref())
+    .pipe(gulpRevReplace())
+    .pipe(gulp.dest(config.build))
+    .pipe(gulpRev.manifest())
+    .pipe(gulp.dest(config.build));
 });
