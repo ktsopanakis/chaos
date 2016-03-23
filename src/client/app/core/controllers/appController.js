1757b7dd4c1b44f87ea81be23c2e77c515430750
Kostas Tsopanakis <Kostas Tsopanakis>
2016-03-24 01:41:19 +0200

    sidebar and quick action bar

--- src/client/app/core/controllers/appController.js
+++ src/client/app/core/controllers/appController.js
@@ -1,3 +1,4 @@
+/* global screenfull */
 (function() {
   'use strict';
 
@@ -12,6 +13,41 @@
 
   function appController($state, $scope, $rootScope) {
     $scope.global = $rootScope;
+    $rootScope.fullScreen = false;
+    if (screenfull.enabled) {
+      document.addEventListener(screenfull.raw.fullscreenchange, function() {
+        $rootScope.fullScreen = screenfull.isFullscreen;
+        $rootScope.$apply();
+      });
+    }
+
+    $rootScope.toggleFullScreen = function() {
+      if (screenfull.enabled) {
+        if ($rootScope.fullScreen) {
+          screenfull.exit();
+        } else {
+          screenfull.request();
+        }
+      }
+    };
+
+    $rootScope.sidebar = true;
+    $rootScope.toggleSidebar = function() {
+      if ($rootScope.sidebar) {
+        $rootScope.sidebar = false;
+      } else {
+        $rootScope.sidebar = true;
+      }
+    };
+
+    $rootScope.quickActions = false;
+    $rootScope.toggleQuickActions = function() {
+      if ($rootScope.quickActions) {
+        $rootScope.quickActions = false;
+      } else {
+        $rootScope.quickActions = true;
+      }
+    };
 
   }
 })();
--- src/client/app/core/controllers/navHeaderController.js
+++ src/client/app/core/controllers/navHeaderController.js
@@ -1,4 +1,3 @@
-/* global screenfull */
 (function(ng) {
   'use strict';
 
@@ -11,43 +10,8 @@
   ];
 
   function navHeaderController($rootScope, $scope) {
-    //TODO: the listener does not handle F11
+    
     $scope.global = $rootScope;
-    $rootScope.fullScreen = false;
-    if (screenfull.enabled) {
-      document.addEventListener(screenfull.raw.fullscreenchange, function() {
-        $rootScope.fullScreen = screenfull.isFullscreen;
-        $rootScope.$apply();
-      });
-    }
-
-    $scope.toggleFullScreen = function() {
-      if (screenfull.enabled) {
-        if ($rootScope.fullScreen) {
-          screenfull.exit();
-        } else {
-          screenfull.request();
-        }
-      }
-    };
-
-    $rootScope.sidebar = true;
-    $scope.toggleSidebar = function() {
-      if ($rootScope.sidebar) {
-        $rootScope.sidebar = false;
-      } else {
-        $rootScope.sidebar = true;
-      }
-    };
-
-    $rootScope.quickActions = false;
-    $scope.toggleQuickActions = function() {
-      if ($rootScope.quickActions) {
-        $rootScope.quickActions = false;
-      } else {
-        $rootScope.quickActions = true;
-      }
-    };
   }
 
 })(angular);
