4d9f8ca7c9f3d38cc9e4360e204649f04f5ff4c9
Kostas Tsopanakis <k.tsopanakis@dotbydot.gr>
2016-04-05 15:48:32 +0300

    logging events, and adding date to logs

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -1,4 +1,4 @@
-/* global _translations */
+/* global _translations,moment */
 (function(ng) {
   'use strict';
 
@@ -42,33 +42,60 @@
   }])
 
   .config([
-    '$stateProvider',
-    '$urlRouterProvider',
-    function($stateProvider, $urlRouterProvider) {
-      $urlRouterProvider.otherwise('/');
-      $stateProvider
-        .state('home', {
-          data: {
-            displayName: 'Home',
-            subTitle: 'Home',
-            requireGuest: false,
-            icon: 'glyphicon-road'
-          },
-          url: '/',
-          templateUrl: 'app/core/layout/home.html',
-          controller: 'homeController'
-        })
-        .state('settings', {
-          data: {
-            displayName: 'Settings',
-            subTitle: 'Settings',
-            requireGuest: false,
-            icon: 'glyphicon-road'
-          },
-          url: '/settings',
-          templateUrl: 'app/core/layout/settings.html',
-          controller: 'settingsController'
-        });
-    }
-  ]);
+      '$stateProvider',
+      '$urlRouterProvider',
+      function($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+          .state('home', {
+            data: {
+              displayName: 'Home',
+              subTitle: 'Home',
+              requireGuest: false,
+              icon: 'glyphicon-road'
+            },
+            url: '/',
+            templateUrl: 'app/core/layout/home.html',
+            controller: 'homeController'
+          })
+          .state('settings', {
+            data: {
+              displayName: 'Settings',
+              subTitle: 'Settings',
+              requireGuest: false,
+              icon: 'glyphicon-road'
+            },
+            url: '/settings',
+            templateUrl: 'app/core/layout/settings.html',
+            controller: 'settingsController'
+          });
+      }
+    ])
+    .config(['$provide', function($provide) {
+      $provide.decorator('$rootScope', ['$delegate', '$log', function($delegate, $log) {
+        var _emit = $delegate.$emit;
+        var _broadcast = $delegate.$broadcast;
+        $delegate.$emit = function() {
+          $log.debug('[$emit] ' + arguments[0] + ' (' + JSON.stringify(arguments) + ')');
+          return _emit.apply(this, arguments);
+        };
+        $delegate.$broadcast = function() {
+          $log.debug('[$broadcast] ' + arguments[0] + ' (' + JSON.stringify(arguments) + ')');
+          return _broadcast.apply(this, arguments);
+        };
+        return $delegate;
+      }]);
+    }])
+    .config(['$provide', function($provide) {
+      $provide.decorator('$log', ['$delegate', function($delegate) {
+        var debugFn = $delegate.debug;
+        $delegate.debug = function() {
+          var args = [].slice.call(arguments);
+          var now = moment().format('mm:ss:SSS');
+          args[0] = '' + now + ' - ' + args[0];
+          debugFn.apply(null, args);
+        };
+        return $delegate;
+      }]);
+    }]);
 }(angular));
